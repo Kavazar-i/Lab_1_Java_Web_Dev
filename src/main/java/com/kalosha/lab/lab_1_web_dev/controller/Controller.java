b1aa7c12567c712106270a03fdb38aaffa33c9b4
Kavazar <zaharkalosha@gmail.com>
2024-03-27 10:58:36 +0200

    Add Controller MVP

---
+++ src/main/java/com/kalosha/lab/lab_1_web_dev/controller/Controller.java
@@ -0,0 +1,39 @@
+package com.kalosha.lab.lab_1_web_dev.controller;
+
+import com.kalosha.lab.lab_1_web_dev.command.Command;
+import com.kalosha.lab.lab_1_web_dev.command.CommandType;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "controller", value = {"/controller", "*.do"})
+public class Controller extends HttpServlet {
+    private String message;
+
+    public void init() {
+
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+//        String strNum = request.getParameter("num");
+//        int resNum = Integer.parseInt(strNum) * 2;
+//        request.setAttribute("result", resNum);
+        String commandStr = request.getParameter("command");
+        Command command = CommandType.define(commandStr);
+        String page = command.execute(request);
+        request.getRequestDispatcher(page).forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    public void destroy() {
+    }
+}
