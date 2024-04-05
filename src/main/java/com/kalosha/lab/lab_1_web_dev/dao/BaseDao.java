6010ea85a88547d083ece28a85ba16307831ba25
Kavazar <zaharkalosha@gmail.com>
2024-04-05 12:52:12 +0300

    Add BaseDao exceptions

--- src/main/java/com/kalosha/lab/lab_1_web_dev/dao/BaseDao.java
+++ src/main/java/com/kalosha/lab/lab_1_web_dev/dao/BaseDao.java
@@ -1,12 +1,13 @@
 package com.kalosha.lab.lab_1_web_dev.dao;
 
 import com.kalosha.lab.lab_1_web_dev.entity.AbstractEntity;
+import com.kalosha.lab.lab_1_web_dev.exeption.DaoExeption;
 
 import java.util.List;
 
 public abstract class BaseDao<T> extends AbstractEntity {
-    public abstract boolean insert(T entity);
-    public abstract boolean delete(T entity);
-    public abstract List<T> findAll();
-    public abstract T update(T entity);
+    public abstract boolean insert(T entity) throws DaoExeption;
+    public abstract boolean delete(T entity) throws DaoExeption;
+    public abstract List<T> findAll() throws DaoExeption;
+    public abstract T update(T entity) throws DaoExeption;
 }
