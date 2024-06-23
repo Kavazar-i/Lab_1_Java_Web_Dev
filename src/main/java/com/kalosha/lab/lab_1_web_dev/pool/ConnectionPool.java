82a823a6495fc2602cbdb15a28ac59b33782f6f8
Kavazar <zaharkalosha@gmail.com>
2024-06-23 22:30:20 +0300

    Improve ConnectionPool

--- src/main/java/com/kalosha/lab/lab_1_web_dev/pool/ConnectionPool.java
+++ src/main/java/com/kalosha/lab/lab_1_web_dev/pool/ConnectionPool.java
@@ -10,86 +10,91 @@ import java.sql.SQLException;
 import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 
 @Log4j
-//@PropertySource("application.properties") todo
 public class ConnectionPool {
     private static ConnectionPool instance;
-    private BlockingQueue<Connection> freeConnectionQueue = new LinkedBlockingQueue<>(POOL_SIZE);
-    private BlockingQueue<Connection> usedConnectionQueue = new LinkedBlockingQueue<>(POOL_SIZE);
+    private BlockingQueue<Connection> freeConnectionQueue;
+    private BlockingQueue<Connection> usedConnectionQueue;
     private static final int POOL_SIZE = 8;
-    public static final String PROPERTIES = "prop/application.properties";
+    public static final String PROPERTIES = "application.properties";
+    private static final Lock lock = new ReentrantLock();
 
     static {
         try {
-//            DriverManager.registerDriver(new org.postgresql.Driver()); todo 55 video 3
-            Class.forName("org.postgresql.Driver");
-//        }  throw new ExceptionInInitializerError(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e); //TODO: end of everything
-//            throw new ExceptionInInitializerError(e); //TODO: end of everything
+            DriverManager.registerDriver(new org.postgresql.Driver());
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to register PostgreSQL driver", e);
         }
     }
 
     private ConnectionPool() {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        InputStream input = classLoader.getResourceAsStream("application.properties");
-// ...
+        freeConnectionQueue = new LinkedBlockingQueue<>(POOL_SIZE);
+        usedConnectionQueue = new LinkedBlockingQueue<>(POOL_SIZE);
+
         Properties properties = new Properties();
-        try {
+        try (InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream(PROPERTIES)) {
+            if (input == null) {
+                throw new RuntimeException("Unable to find " + PROPERTIES);
+            }
+
             properties.load(input);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to load database properties", e);
         }
+
+        String url = properties.getProperty("db.url");
+        String user = properties.getProperty("db.user");
+        String password = properties.getProperty("db.password");
+
         for (int i = 0; i < POOL_SIZE; i++) {
             try {
-                String url = properties.getProperty("db.url");
-                String user = properties.getProperty("db.user");
-                String password = properties.getProperty("db.password");
                 Connection connection = DriverManager.getConnection(url, user, password);
                 freeConnectionQueue.add(connection);
             } catch (SQLException e) {
-                log.error("Connection failed: %s", e);
-                throw new ExceptionInInitializerError(e); //TODO: end of everything softer можно все в фор с логом и потом если конекшнов меньше 8 досоздать
+                log.error("Failed to create connection: ", e);
+                throw new ExceptionInInitializerError(e);
             }
         }
     }
 
     public static ConnectionPool getInstance() {
-        //TODO: lock
-        instance = new ConnectionPool();
-        //TODO: unlock
+        if (instance == null) {
+            lock.lock();
+            try {
+                if (instance == null) {
+                    instance = new ConnectionPool();
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
         return instance;
     }
 
     public Connection getConnection() {
-        Connection connection = null; //TODO
         try {
-            connection = freeConnectionQueue.take();
+            Connection connection = freeConnectionQueue.take();
             usedConnectionQueue.put(connection);
+            return connection;
         } catch (InterruptedException e) {
-            log.error("Connection failed: %s", e);
+            log.error("Failed to get connection", e);
             Thread.currentThread().interrupt();
+            return null;
         }
-        return connection;
     }
 
     public void releaseConnection(Connection connection) {
         try {
-            usedConnectionQueue.remove(connection);
-            freeConnectionQueue.put(connection);
+            if (usedConnectionQueue.remove(connection)) {
+                freeConnectionQueue.put(connection);
+            }
         } catch (InterruptedException e) {
-            log.error("Connection failed: %s", e);
+            log.error("Failed to release connection", e);
             Thread.currentThread().interrupt();
-        }
-    }
-
-    //TODO: deregister driver
-    public void deregisterDriver() {
-        try {
-            DriverManager.deregisterDriver(new org.postgresql.Driver());
-        } catch (SQLException e) {
-            log.error("Connection failed: %s", e);
         }
     }
 
@@ -98,8 +103,17 @@ public class ConnectionPool {
             try {
                 freeConnectionQueue.take().close();
             } catch (SQLException | InterruptedException e) {
-                log.error("Connection failed: %s", e);
+                log.error("Failed to close connection", e);
             }
+        }
+        deregisterDriver();
+    }
+
+    private void deregisterDriver() {
+        try {
+            DriverManager.deregisterDriver(new org.postgresql.Driver());
+        } catch (SQLException e) {
+            log.error("Failed to deregister driver", e);
         }
     }
 }
