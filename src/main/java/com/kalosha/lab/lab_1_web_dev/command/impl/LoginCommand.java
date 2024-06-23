9d4bdd327e3fb35cf0d436ae4a66f144e0ec6134
Kavazar <zaharkalosha@gmail.com>
2024-06-23 15:38:00 +0300

    Add new content in login logout commands

--- src/main/java/com/kalosha/lab/lab_1_web_dev/command/impl/LoginCommand.java
+++ src/main/java/com/kalosha/lab/lab_1_web_dev/command/impl/LoginCommand.java
@@ -6,6 +6,7 @@ import com.kalosha.lab.lab_1_web_dev.exeption.ServiceExeption;
 import com.kalosha.lab.lab_1_web_dev.service.UserService;
 import com.kalosha.lab.lab_1_web_dev.service.impl.UserServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 public class LoginCommand implements Command {
     @Override
@@ -14,15 +15,19 @@ public class LoginCommand implements Command {
         String password = request.getParameter("pass"); //TODO: To constants
         UserService userService = UserServiceImpl.getInstance();
         String page;
+        HttpSession session = request.getSession();
+
 
         try {
             if(userService.authenticate(username, password)) {
                 request.setAttribute("user", username);
+                session.setAttribute("user_name", username);
                 page = "pages/main.jsp"; //TODO: To constants (PagePasses class?)
             } else {
                 request.setAttribute("login_error_msg", "Invalid username or password"); //TODO: To constants (PagePasses class?)
                 page = "index.jsp"; //TODO: To constants (PagePasses class?)
             }
+            session.setAttribute("current_page", page);
         } catch (ServiceExeption e) {
             throw new CommandExeption(e);
         }
