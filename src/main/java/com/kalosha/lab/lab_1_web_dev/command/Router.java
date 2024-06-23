39bfea91565d082bd7170d90e30265983b3ee310
Kavazar <zaharkalosha@gmail.com>
2024-06-23 15:38:36 +0300

    Add Router

---
+++ src/main/java/com/kalosha/lab/lab_1_web_dev/command/Router.java
@@ -0,0 +1,26 @@
+package com.kalosha.lab.lab_1_web_dev.command;
+
+public class Router {
+    private String page;
+    private Type type = Type.FORWARD;
+    enum Type {
+        FORWARD, REDIRECT
+    }
+
+    public Router(String page, Type type) {
+        this.page = page;
+        this.type = type;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+    public void setRedirect() {
+        this.type = Type.REDIRECT;
+    }
+}
