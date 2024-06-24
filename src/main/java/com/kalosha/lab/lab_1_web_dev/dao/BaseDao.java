48079b72ebc725a539c5c428807ef0f348885ac2
Kavazar <zaharkalosha@gmail.com>
2024-06-24 11:18:29 +0300

    Add renaming

--- src/main/java/com/kalosha/lab/lab_1_web_dev/dao/BaseDao.java
+++ src/main/java/com/kalosha/lab/lab_1_web_dev/dao/BaseDao.java
@@ -1,13 +1,13 @@
 package com.kalosha.lab.lab_1_web_dev.dao;
 
-import com.kalosha.lab.lab_1_web_dev.exeption.DaoException;
+import com.kalosha.lab.lab_1_web_dev.exception.DaoException;
 
 import java.util.List;
 
 public abstract class BaseDao<T> {
     public abstract List<T> findAll() throws DaoException;
 
-    public abstract T create(T entity) throws DaoException;
+    public abstract T save(T entity) throws DaoException;
 
     public abstract T update(T entity) throws DaoException;
 
