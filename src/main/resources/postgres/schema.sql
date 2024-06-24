f3a9546ca3dcf8d5f53a64c76362bf818526b010
Kavazar <zaharkalosha@gmail.com>
2024-06-24 11:26:42 +0300

    Final schema.sql

--- src/main/resources/postgres/schema.sql
+++ src/main/resources/postgres/schema.sql
@@ -1,19 +1,20 @@
-CREATE TABLE users (
-                       id INT AUTO_INCREMENT PRIMARY KEY,
-                       username VARCHAR(50) UNIQUE NOT NULL,
-                       password VARCHAR(255) NOT NULL,
-                       fullname VARCHAR(100),
-                       email VARCHAR(100),
-                       bio TEXT,
-                       skills VARCHAR(255)
+CREATE TABLE users
+(
+    id       SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255)       NOT NULL,
+    fullname VARCHAR(100),
+    email    VARCHAR(100),
+    bio      TEXT,
+    skills   VARCHAR(255)
 );
 
-CREATE TABLE projects (
-                          id INT AUTO_INCREMENT PRIMARY KEY,
-                          user_id INT NOT NULL,
-                          title VARCHAR(100) NOT NULL,
-                          description TEXT,
-                          image_url VARCHAR(255),
-                          project_url VARCHAR(255),
-                          FOREIGN KEY (user_id) REFERENCES users(id)
+CREATE TABLE project
+(
+    id             SERIAL PRIMARY KEY,
+    user_id        INTEGER REFERENCES users (id) NOT NULL,
+    title          VARCHAR(255)                  NOT NULL,
+    description    TEXT,
+    project_url    VARCHAR(255),
+    photo_filename VARCHAR(255)
 );
